ef4e8f168118a56ba8403c6dd2643d6bf440e0c1
Ygor Azambuja <ygorazambuja@gmail.com>
2020-11-21 20:31:20 -0400

    implementação de controlador generico

---
+++ src/main/java/com/ufms/olx/controllers/GenericController.java
@@ -0,0 +1,11 @@
+package com.ufms.olx.controllers;
+
+import org.springframework.http.ResponseEntity;
+
+public interface GenericController<Entity, DTO> {
+    public ResponseEntity<?> addEntity(DTO entitie);
+    public ResponseEntity<?> removeEntity(Long id);
+    public ResponseEntity<?> updateEntity(Entity entity, Long id);
+    public ResponseEntity<?> getAllEntities();
+    public ResponseEntity<?> getEntityById(Long id);
+}
